731e5c4aad47b7638d90c3b6423328f6729dc8a4
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-06 22:59:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,67 +1,74 @@
 class Song
   attr_accessor :name, :artist_name
-  @@all = []	
-  self.class.all << self
+  @@all = []
+
+  def self.all
+    @@all
   end
 
+  def save
+    self.class.all << self
+  end
 
   def self.create
     song = self.new
-    song.save
+    #@@all << song
     song
-  end 
-
+  end
+  
   def self.new_by_name(name)
-    song = self.new
+    song = self.new 
     song.name = name
     song
-  end 
-
+  end
+  
   def self.create_by_name(name)
-    song = self.create
-    song.name = name 
+    song = self.new 
+    song.name = name
+    @@all << song
     song
-  end 
-
+  end
+  
   def self.find_by_name(name)
-    @@all.each do |song|
-      if song.name == name
-        return song
-      end
-
+    @@all.find{|x| x.name == name}
+  end
+  
   def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create_by_name(name)
+      #if self.find_by_name(name) == nil 
+        #self.create_by_name(name)
+      #else
+        #self.find_by_name(name)
+      #end
+      self.find_by_name(name) || self.create_by_name(name)
+      #the above statement is saying do this(if it is true) or that (if the first thing is not true and the second thing is true)
   end
-
-
-  def self.alphabetical
-
-    @@all.sort_by{|song| song.name}
-
+  
+  def self.alphabetical()
+    #returns all the songs instances in ascending (a-z) alphabetical order.
+    @@all.sort_by{|x| x.name}
   end
-
-  def self.new_from_filename(filename)
-
-    row = filename
-
-      data = row.split(" - ")
-      artist_name = data[0]
-      song_name = data[1].gsub(".mp3", "")
-
+  
+  
+  def self.new_from_filename(name)
+    song = self.new 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
+  
+  def self.create_from_filename(name)
+  #class method should not only parse the filename correctly but should also save the song
     song = self.new
-    song.name = song_name
-    song.artist_name = artist_name
-    song
-end
-
-
-
-
-
-  def self.create_from_filename(filename)
-    song = self.new_from_filename(filename)
-    song.save
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song
     song
   end
+  
+  def self.destroy_all()
+    #reset the state of the @@all class variable to an empty array thereby deleting all previous song instances.
+    @@all.clear
+  end
 
+end
   
